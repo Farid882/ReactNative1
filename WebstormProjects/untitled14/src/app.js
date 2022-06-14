a67c6d1939949c0595c460fc4de93bcde051cf08
DESKTOP-7P5M41M\subfa <sabfarid@mail.ru>
2022-06-14 10:34:41 +0300

    Create React app

---
+++ WebstormProjects/untitled14/src/app.js
@@ -0,0 +1,17 @@
+<meta
+  name="description"
+  content="Web site created using create-react-app"
+/>
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Мой заголовок</h1>
+        <button>Отправить</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
